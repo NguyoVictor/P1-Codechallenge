e6b685920f823021c08edb0d27415e8f5d8426c8
Victor Nguyo <victor.nguyo@student.moringaschool.com>
2024-03-25 18:57:19 +0300

    codechallenge

---
+++ gradeGenerator.js
@@ -0,0 +1,36 @@
+///initially we start with prompting the user to put in the marks
+
+const prompt = require("prompt-sync")();
+const studMarks = prompt("Enter the students marks: ");
+
+//we now come up with a function that evaluates the grades
+function studentGrade(marks) {
+  //Creating an if statement
+  if (marks > 79 && marks <= 100) {
+    let grade = "A";
+    console.log(`Grade: ${grade}`);
+    return grade;
+  } else if (marks >= 60 && marks <= 79) {
+    let grade = "B";
+    console.log(`Grade: ${grade}`);
+    return grade;
+  } else if (marks >= 49 && marks <= 59) {
+    let grade = "C";
+    console.log(`Grade: ${grade}`);
+    return grade;
+  } else if (marks >= 40 && marks < 49) {
+    let grade = "D";
+    console.log(`Grade: ${grade}`);
+    return grade;
+  } else if (marks < 40 && marks >= 0) {
+    let grade = "E";
+    console.log(`Grade: ${grade}`);
+    return grade;
+  } else {
+    console.log("out of range");
+  }
+}
+
+studentGrade(studMarks);
+console.log(marks);
+
---
+++ speedDetector.js
@@ -0,0 +1,47 @@
+const prompt = require("prompt-sync")();
+//this is the maximum allowed speed limit
+const limit = 70;
+let totalPoints = 0;
+const demeritPoints = 12; //maximum points you need to have before your license is revocked
+//license is revoked
+const suspendMsg = "License Suspended"; //the license suspended message
+
+//function that returns a message according to the speed
+function getStatus() {
+  while (totalPoints <= demeritPoints) {
+    let speed = prompt("Enter the speed: "); //prompts to input the speed
+    if (speed <= limit) {
+      console.log("YOU ARE A BLESSING TO ROAD USERS");
+      condition = prompt("Proceed?");
+      if (
+        condition.toLowerCase() === "yes"
+      ) {
+        continue; //starts all over if typed in yes
+      } else {
+        break;// the loop is discontinued
+      }
+    } else {
+      let currentPoints = (speed - limit) / 5;
+      totalPoints += currentPoints;
+      if (totalPoints <= demeritPoints) {
+        console.log(`Points: ${currentPoints}`);
+        console.log(`Total Points: ${totalPoints}`);
+        condition = prompt("Proceed? ");
+        if (
+          condition.toLowerCase() === "yes"
+        ) {
+          continue; //if the user chooses to continue the loop starts over
+        } else {
+          console.log(`Total Points: ${totalPoints}`); //logs total points
+          break;
+        }
+      } else {
+        console.log(`Points: ${currentPoints}`); //displays the points calculated from the speed that caused the total points to surpass the allowed demerit points
+        console.log(`Total Points: ${totalPoints}`); //if the total points are more than the demerit points
+        console.log(suspendMsg); // outputs the license suspended message
+      }
+    }
+  }
+}
+
+getStatus();
